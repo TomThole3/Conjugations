0f3b0ba777b6884662da86be8d739d89ffba5b4b
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-05-21 11:30:53 +0200

    added verb type selection io

--- main.py
+++ main.py
@@ -4,33 +4,25 @@ Created on Thu May 21 10:18:36 2026
 
 @author: tthol
 """
-
-class Main:
-    def main(self):
-        io = InputOutput()
-        db = DatabaseHandling()
-        app = Conjugar(io)
-        app.run()
-        
 class Conjugar:
     def __init__(self, io):
         self.io = io
     
     def run(self):
-        pass
+        tenses = self.io.get_tenses()
+        verb_type = self.io.verb_type()
     
 class InputOutput:
-    def get_settings():
+    
+    def get_tenses(self):
         valid_tenses = ("present", "present progressive", "preterite", "imperfect", "future",
                         "conditional", "present perfect", "pluperfect", "future perfect", "conditional perfect",
                         "present subjunctive", "imperfect subjunctive", "present perfect subjunctive",
                         "pluperfect subjunctive", "past progressive", "future progressive" "imperative affirmative",
                         "negative imperative")
 
-        print(
-        "What tenses would you like to cover?"
+        print("What tenses would you like to cover?")
         
-        )
         while True:
             tenses = input().lower().split(",")
             stripped_set = set([tense.strip() for tense in tenses])
@@ -38,10 +30,32 @@ class InputOutput:
                 break
             else:
                 print("Invalid input")
+        return stripped_set
+    
+    def verb_type(self):
+        while True:
+            type_ = input("Do you want to select verbs based on ending, power or name? ")
+            type_ = type_.strip().lower()
+            if type_ == "ending" or type_ == "power" or type_ == "name":
+                return type_
+            print("Invalid input")
+        
             
-    def get_verbs():
+    def get_verbs(self):
         pass
     
 class DatabaseHandling:
     pass
+
+
+def main():
+    io = InputOutput()
+    db = DatabaseHandling()
+    app = Conjugar(io)
+    app.run()
+    
+if __name__ == "__main__":
+    main()
+    
+
         
