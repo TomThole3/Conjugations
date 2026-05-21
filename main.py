56eb277dde799c0ae7afbeea30968d4ca6e36da3
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-05-21 10:39:18 +0200

    added basic class structure

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ Created on Thu May 21 10:18:36 2026
 @author: tthol
 """
 
+class Main:
+    def main(self):
+        io = InputOutput()
+        db = DatabaseHandling()
+        app = Conjugar(io)
+        app.run()
+        
+class Conjugar:
+    def __init__(self, io):
+        self.io = io
+    
+    def run(self):
+        pass
+    
+class InputOutput:
+    pass
+
+class DatabaseHandling:
+    pass
+        
