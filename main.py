cefe9148c74f202f8cc16af21bf3ef589b25c95c
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-03 08:05:25 +0200

    added statistics during questions

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ class Conjugar:
     def __init__(self, io, db):
         self.io = io
         self.db = db
+        self.total_words = 0
+        self.passed = 0
     
     def run(self):
         tenses, endings, powers, infinitives = (self.io.get_filters(
             self.db.get_unique_values(field), field) for field in ('tense', 'ending', 'power', 'infinitive'))
         verbs = self.db.get_filtered_entries(tenses, endings, powers, infinitives)
         rand.shuffle(verbs)
+        self.total_words = len(verbs)
         self.gameloop(verbs)
         
     def gameloop(self, verb_list, repeat = True):
@@ -29,15 +32,19 @@ class Conjugar:
         while verb_list:
             self.io.clear_console()
             infinitive, tense, person, answer = verb_list[0]
+            self.io.print_stats(self.total_words, self.passed+1)
             user_answer = self.io.print_question(infinitive, tense, person)
             wrong_index = min(3, len(verb_list))
             if user_answer != answer:
                 verb_list.insert(wrong_index, verb_list[0])
+                self.total_words += 1
                 check = self.io.print_correct(answer)
                 if len(check) > 0:
                     wrong.pop(0)
                     verb_list.pop(max(0, wrong_index-1))
                 wrong.append(verb_list[0])
+            else:
+                self.passed += 1
             verb_list.pop(0)
         if repeat:
             self.gameloop(wrong, False)
@@ -70,6 +77,9 @@ class InputOutput:
     def print_correct(self, answer):
         print(f"The correct answer was: {answer}")
         return input("Press enter to skip, type anything to rectify answer ")
+    
+    def print_stats(self, total, passed):
+        print(f'Word {passed}/{total}')
         
     def clear_console(self):
         try:
