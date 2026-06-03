4bb6c619c8080ff369ba90755fd8a209d84d8b5e
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-03 09:03:53 +0200

    improved reusability of connection code and initialized variables

--- database_handling.py
+++ database_handling.py
@@ -7,25 +7,26 @@ import pandas as pd
 from itertools import chain
 
 class DatabaseHandling:
-    
+
     def __init__(self):
-        pass
-        
-    def excel_to_db(self):
         cwd = Path.cwd()
-        path = cwd.parents[0]
-        path = os.path.join(path, "verbs excel.xlsx")
-        ex = pd.read_excel(path)
-        with sqlite3.connect('verbs.db') as conn:
+        self.db_path = cwd / 'verbs.db'
+        self.excel_path = cwd.parents[0] / 'verbs excel.xlsx'
+
+    def get_connection(self):
+        return sqlite3.connect(self.db_path)
+
+    def excel_to_db(self):
+        ex = pd.read_excel(self.excel_path)
+        with self._get_connection() as conn:
             ex.to_sql('Blad1', conn, if_exists='replace')
-    
+
     def get_unique_values(self, column):
-        with sqlite3.connect('verbs.db') as conn:
+        with self._get_connection() as conn:
             c = conn.cursor()
-            queried_names = c.execute(f"""SELECT DISTINCT {column} FROM Blad1""")
-            names = queried_names.fetchall()
-        return [name for (name, ) in names]
-    
+            queried_names = c.execute(f"SELECT DISTINCT {column} FROM Blad1")
+            return [name for (name,) in queried_names.fetchall()]
+
     def get_filtered_entries(self, tenses, endings, powers, infinitives):
         tense_placeholder = ', '.join('?' for _ in tenses)
         ending_placeholder = ', '.join('?' for _ in endings)
@@ -36,12 +37,11 @@ class DatabaseHandling:
                     AND ending IN ({ending_placeholder})
                     AND power IN ({power_placeholder})
                     AND infinitive IN ({infinitive_placeholder})"""
-        with sqlite3.connect('verbs.db') as conn:
+        with self.get_connection() as conn:
             c = conn.cursor()
             queried_entries = c.execute(query, tuple(chain.from_iterable((tenses, endings, powers, infinitives))))
-            names = queried_entries.fetchall()
-        return names
-    
+            return queried_entries.fetchall()
+
     def get_verb(self, infinitive):
         with sqlite3.connect('verbs.db') as conn:
             c = conn.cursor()
